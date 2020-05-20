1ea449d286f25a436e50ef026608edeca26f7408
unknown <yutasasagawa898@gmail.com>
2020-05-19 23:33:57 -0300

    implementendo logica para o formulario

--- src/conversor-moedas.js
+++ src/conversor-moedas.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './conversor-moedas.css'
 import { Jumbotron, Button, Form, Col, Spinner, Alert, Modal } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,20 +7,45 @@ import ListarMoedas from './listar-moedas';
 
 
 function ConversorMoedas() {
+
+  const [valor, setValor] = useState('1');
+  const [moedaDe, setMoedaDe] = useState('BRL');
+  const [moedaPara, setMoedaPara] = useState('USD');
+  const [exibirSpinner, setExibirSpinner] = useState(false);
+  const [formValidado, setFormValidado] = useState(false);
+
+  function handleValor(event) {
+    setValor(event.target.value.replace(/\D/g, ''));
+  }
+  function handleMoedaDe(event) {
+    setMoedaDe(event.target.value);
+  }
+  function handleMoedaPara(event) {
+    setMoedaPara(event.targer.value);
+  }
+  function converter(event) {
+    event.preventDefault();
+    setFormValidado(true);
+    if(event.currentTarget.checkValidity() === true) {
+      //TODO implementar a chamada do fixer.io
+    }
+  }
+
+
   return (
     <div>
       <h1>Conversor de Moedas</h1>
       <Alert variant="danger" show={false}>Erro obtendo dados de convesão, tente novamente.</Alert>
       <Jumbotron>
-        <Form>
+        <Form onSubmit={converter} noValidate validated={formValidado}>
           <Form.Row>
-            <Col sm="3"><Form.Control placeholder="0" value={1} required /></Col>
-            <Col sm="3"><Form.Control as="select"><ListarMoedas /></Form.Control></Col>
+            <Col sm="3"><Form.Control placeholder="0" value={valor} onChange={handleValor} required /></Col>
+            <Col sm="3"><Form.Control as="select" value={moedaDe} onChange={handleMoedaDe}><ListarMoedas /></Form.Control></Col>
             <Col sm="1" className="text-center mt-2"><FontAwesomeIcon icon={faAngleDoubleRight} /></Col>
-            <Col sm="3"><Form.Control as="select"><ListarMoedas /></Form.Control></Col>
+            <Col sm="3"><Form.Control as="select" value={moedaPara} onChange={handleMoedaPara}><ListarMoedas /></Form.Control></Col>
             <Col sm="2"><Button variant="success" type="submit">
-              <Spinner animation="border" size="sm" />
-              Converter
+              <span className={exibirSpinner ? null : 'hidden'}><Spinner animation="border" size="sm" /></span>
+              <span className={exibirSpinner ? 'hidden' : null}>Converter</span>
               </Button></Col>
           </Form.Row>
         </Form>
